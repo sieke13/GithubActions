664821bd4839be3d005f4fc80d3ad18a6496d56e
sieke13 <gera.ave2@gmail.com>
2025-03-07 12:02:14 -0600

    new

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,12 +3,16 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    if (models[modelName]) {
+      let modelExists = await models[modelName].db.db.listCollections({
+        name: collectionName
+      }).toArray();
 
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
+      if (modelExists.length) {
+        await db.dropCollection(collectionName);
+      }
+    } else {
+      throw new Error(`${modelName} does not exist in models.`);
     }
   } catch (err) {
     throw err;
