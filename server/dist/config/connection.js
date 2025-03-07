a51a86333a4a482acb7a9d3447a9311d14c3e915
sieke13 <gera.ave2@gmail.com>
2025-03-07 15:51:46 -0600

    1

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -1,5 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/techquiz', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 export default mongoose.connection;
